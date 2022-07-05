62142ad93d6fff43602da7f2a40eb3bb0a905ee0
UsielOM <usiel31197@outlook.es>
2022-07-04 20:20:48 -0500

    Creamos una tabla nueva o modelo

---
+++ app/models/Table/Prueba.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require('../configBd');
+
+const Prueba = sequelize.define('prueba', {
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    user_name: {
+        type: Sequelize.DataTypes.STRING,
+
+    },
+    user_password: {
+        type: Sequelize.DataTypes.STRING,
+
+    },
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
+module.exports = Prueba;
